30236b3f90dd31343ab39a6ada35bede76ce14c0
Mark Huk <mark.guk.e@gmail.com>
2020-04-03 23:18:12 +0200

    Create 20-lcd.rb

---
+++ 20-lcd.rb
@@ -0,0 +1,69 @@
+
+gem 'ffi-wiring_pi'
+
+require 'ffi/wiring_pi'
+
+FFI::WiringPi::GPIO.setup
+
+include FFI::WiringPi::GPIO
+extend FFI::WiringPi::GPIO
+define_method :get, &FFI::WiringPi::GPIO.method(:get)
+define_method :write, &FFI::WiringPi::GPIO.method(:write)
+define_method :read, &FFI::WiringPi::GPIO.method(:read)
+define_method :analog_read, &FFI::WiringPi::GPIO.method(:analog_read)
+
+FFI::WiringPi::GPIO.set_pin_mode $data_pin, FFI::WiringPi::GPIO::OUTPUT
+FFI::WiringPi::GPIO.set_pin_mode $latch_pin, FFI::WiringPi::GPIO::OUTPUT
+FFI::WiringPi::GPIO.set_pin_mode $clock_pin, FFI::WiringPi::GPIO::OUTPUT
+
+FFI::WiringPi::LCD.setup
+
+#define pcf8574_address 0x27 // default I2C address of Pcf8574
+ADDRESS = 0x3F # default I2C address of Pcf8574A
+BASE = 64 # BASE is not less than 64
+# Define the output pins of the PCF8574, which are directly connected to the
+# LCD1602 pin.
+RS = BASE+0
+RW = BASE+1
+EN = BASE+2
+LED = BASE+3
+D4 = BASE+4
+D5 = BASE+5
+D6 = BASE+6
+D7 = BASE+7
+
+require 'file'
+
+def print_cpu_temperature(display)
+ 
+ # CPU temperature data is stored in this directory.
+ fp = File.read("/sys/class/thermal/thermal_zone0/temp", "r")
+ temp = fp.to_f / 1000.0
+ puts("CPU's temperature : #{temp.ceil(2)}"
+ display.set_position(0, 0)
+ display.print(format("CPU:%.2fC",temp)) #Display CPU temperature on LCD
+end
+
+def print_date(display) 
+ display.set_position(0, 1)
+ time = Time.now
+ display.print(format("Time:%d:%d:%d",time.hour, time.minute, time.second)
+end
+ 
+ FFI::WiringPi::Pcf8574.setup BASE, ADDRESS
+ 
+8.times do |i|
+  FFI::WiringPi::GPIO.set_pin_mode(BASE + i, FFI::WiringPi::GPIO::OUTPUT)
+end
+ 
+write LED, true # turn on LCD backlight
+write RW, false # allow writing to LCD
+# initialize LCD and return display instance
+display = FFI::WiringPi::LCD.new(2, 16, 4, RS, EN, D4, D5, D6, D7, 0, 0, 0, 0) 
+
+loop do
+  print_cpu_temperature display
+  print_date display
+  sleep 1 
+end
+ 
