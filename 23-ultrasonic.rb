7b5c31816b075dba3b17a851b227fd7f7d6293bd
Mark Huk <mark.guk.e@gmail.com>
2020-04-05 20:39:59 +0200

    Create 23-ultrasonic.rb

---
+++ 23-ultrasonic.rb
@@ -0,0 +1,50 @@
+
+gem 'ffi-wiring_pi'
+
+require 'ffi/wiring_pi'
+
+FFI::WiringPi::GPIO.setup
+
+include FFI::WiringPi::GPIO
+extend FFI::WiringPi::GPIO
+define_method :get, &FFI::WiringPi::GPIO.method(:get)
+define_method :write, &FFI::WiringPi::GPIO.method(:write)
+define_method :read, &FFI::WiringPi::GPIO.method(:read)
+define_method :analog_read, &FFI::WiringPi::GPIO.method(:analog_read)
+
+
+TRIGGER = 4
+DATA = 5
+
+MAX_DISTANCE = 220#        // define the maximum measured distance
+TIMEOUT = MAX_DISTANCE * 60 #// calculate timeout according to the maximum measured distance
+
+def pulse_in(pin)
+  start = Time.now
+  until read(pin)
+    sleep 0.000001
+  end
+  time = Time.now
+  while read(pin)
+    sleep 0.000001
+  end  
+  Time.now - time
+end
+
+def get_sonar   #get the measurement result of ultrasonic module with unit: cm
+    write TRIGGER, true
+    sleep 0.00001 
+    write TRIGGER, false
+    pingTime = pulseIn(DATA)
+    pingTime * 340.0 / 2.0 / 10000.0
+end
+
+FFI::WiringPi::GPIO.set_pin_mode TRIGGER, FFI::WiringPi::GPIO::OUTPUT
+FFI::WiringPi::GPIO.set_pin_mode DATA, FFI::WiringPi::GPIO::INPUT
+
+loop do
+  p get_sonar
+  sleep 0.5 
+end
+
+
